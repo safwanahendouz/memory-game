baf8d9141f0c6d74d06439c599d2ff27b03bc7e1
safwanahendouz <safwanahendouz241@gmail.com>
2026-02-22 14:18:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const moves = document.getElementById("moves-count");
+const timeValue = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const gameContainer = document.querySelector(".game-container");
+const result = document.getElementById("result");
+const controls = document.querySelector(".controls-container");
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+
+// Items array
+const items = [
+    { name: "bee", image: "bee.png"},
+    { name: "crocodile", image: "crocodile.png"},
+    { name: "macaw", image: "macaw.png"},
+    { name: "gorilla", image: "gorilla.png"},
+    { name:"tiger", image: "tiger.png"},
+    { name:"monkey", image: "monkey.png"},
+    { name:"chameleon", image: "chameleon.png"},
+    { name:"piranha", image: "piranha.png"},
+    { name:"anaconda", image: "anaconda.png"},
+    { name:"sloth", image: "sloth.png"},
+    { name:"cockatoo", image: "cockatoo.png"},
+    { name:"toucan", image: "toucan.png"},
+];
+
+// initial Time
+let seconds = 0,
+minutes = 0;
+
+// Initial moves and win count
+let movesCount = 0,
+winCount = 0;
+
+// For timer
+const tomeGenerator = () => {
+    seconds += 1;
+    // minutes logic
+    if(seconds>= 60) {
+        minutes+= 1;
+        seconds= 0;
+    }
+}
+
+// format time before displaying
+let secondsValue = seconds < 10
